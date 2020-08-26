a74d8957df9960c03a15e593b0693c5a592c1239
KorbikKamil <korbik.gm@gmail.com>
2020-08-26 20:56:43 +0200

    Add Race constructor and initialize method

--- src/Race.php
+++ src/Race.php
@@ -3,5 +3,18 @@
 namespace Project;
 
 class Race {
+    private $distance = 0.0;
 
+    public function __construct(float $distance = 5)
+    {
+        $this->distance = $distance;
+    }
+
+    public function run() : void {
+        $this->displayInfo();
+    }
+
+    public function displayInfo() : void {
+        echo "\n Distance: \t $this->distance";
+    }
 }
