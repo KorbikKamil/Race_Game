a94311691497ffb88a03c55b9e4e667dc77963f3
KorbikKamil <korbik.gm@gmail.com>
2020-08-26 21:00:58 +0200

    Add displaying methods and display for each tour

--- src/Race.php
+++ src/Race.php
@@ -2,19 +2,32 @@
 
 namespace Project;
 
-class Race {
+class Race
+{
     private $distance = 0.0;
+    private $maxTours = 5;
 
     public function __construct(float $distance = 5)
     {
         $this->distance = $distance;
     }
 
-    public function run() : void {
+    public function run(): void
+    {
         $this->displayInfo();
+
+        foreach (range(1, $this->maxTours) as $tour) {
+            $this->displayTourInfo($tour);
+        }
     }
 
-    public function displayInfo() : void {
-        echo "\n Distance: \t $this->distance";
+    public function displayInfo(): void
+    {
+        echo "\n Distance: \t {$this->distance}";
+    }
+
+    private function displayTourInfo(int $tour): void
+    {
+        echo "\n Tour {$tour} began:";
     }
 }
