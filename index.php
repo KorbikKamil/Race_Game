3687d4195a9a3823bc97bd943f5a516bfb717e28
KorbikKamil <korbik.gm@gmail.com>
2020-08-27 18:22:51 +0200

    Implement builder pattern

--- index.php
+++ index.php
@@ -5,12 +5,32 @@ include_once __DIR__ . '/vendor/autoload.php';
 use \Project\Race;
 use \Project\Weather;
 use \Project\VehicleFactory;
+use \Project\VehicleBuilder;
+
+$builder = new VehicleBuilder();
+$builder->setType(VehicleBuilder::CAR);
+$builder->setName('abc');
+$builder->build();
 
 $weather = new Weather();
 
 $race = new Race($weather);
-$race->addVehicle(VehicleFactory::factory(VehicleFactory::CAR, 'abCD'));
-$race->addVehicle(VehicleFactory::factory(VehicleFactory::CAR, 'cdEF'));
-$race->addVehicle(VehicleFactory::factory(VehicleFactory::TRUCK, 'ghi'));
-$race->addVehicle(VehicleFactory::factory(VehicleFactory::MOTORCYCLE, 'zaW4'));
+
+$builder->setType(VehicleBuilder::CAR);
+$builder->setName('abc');
+$builder->build();
+
+$race->addVehicle($builder->build());
+
+$builder->setName('cde');
+$race->addVehicle($builder->build());
+
+$builder->setType(VehicleBuilder::TRUCK);
+$builder->setName('ghi');
+$race->addVehicle($builder->build());
+
+$builder->setType(VehicleBuilder::MOTORCYCLE);
+$builder->setName('jkl');
+$race->addVehicle($builder->build());
+
 $race->run();
