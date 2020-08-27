d741cf61c5b304124f2601634aedd2ad8bb0ccff
KorbikKamil <korbik.gm@gmail.com>
2020-08-27 15:48:19 +0200

    Refactor index.php and Race.php

--- index.php
+++ index.php
@@ -4,15 +4,13 @@ include_once __DIR__ . '/vendor/autoload.php';
 
 use \Project\Race;
 use \Project\Weather;
-use \Project\Car;
-use \Project\Truck;
-use \Project\Motorcycle;
+use \Project\VehicleFactory;
 
 $weather = new Weather();
 
 $race = new Race($weather);
-$race->addVehicle(new Car(1));
-$race->addVehicle(new Car(2));
-$race->addVehicle(new Car(3));
-$race->addVehicle(new Motorcycle(1));
+$race->addVehicle(VehicleFactory::factory(VehicleFactory::CAR, 1));
+$race->addVehicle(VehicleFactory::factory(VehicleFactory::CAR, 2));
+$race->addVehicle(VehicleFactory::factory(VehicleFactory::CAR, 3));
+$race->addVehicle(VehicleFactory::factory(VehicleFactory::MOTORCYCLE, 1));
 $race->run();
